f5641f2d4b6adba03d9d543afaeceddba1a4d099
Robert Lesser <rdlesser@gmail.com>
2019-09-11 17:48:27 +0300

    End of lesson 5

---
+++ Assets/Dead Earth/Scripts/AIWaypointNetwork.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIWaypointNetwork : MonoBehaviour
+{
+    public List<Transform> Waipoints = new List<Transform>();
+    
+}
